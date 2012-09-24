f9da138bceb097cad9d231e45c575c4dba0e0905
Matthias Mees <mm@yellowled.de>
2012-09-24 11:29:39 +0200

    Added AJAX lazyload function with usage example.

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,25 @@ $(function() {
 		footer: '#footer'
 	});
 });
+// Won't work locally without a webserver
+$.getScript = function(url, callback){
+	$.ajax({
+		type: "GET",
+		url: url,
+		success: callback,
+		dataType: "script",
+		cache: true
+	});
+};
+// Usage example
+// $(window).load(function(){
+// 	// Lazyload condition
+// 	var $hasTooltips = $('#main').has('.tooltips');
+// 	if($hasTooltips.size() > 0) {
+// 		// Relative to page, not script; can be used without function to just load
+// 		$.getScript('scripts/plugins/jquery.tipsy.js', function() {
+// 			// Fire plugin code here
+// 			$('.tooltips a[title]').tipsy({ gravity: 's' });
+// 		});
+// 	}
+// });
