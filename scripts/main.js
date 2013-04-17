a121b56add198f641143cc0c36318d71d6965ee0
Matthias Mees <mm@yellowled.de>
2013-04-17 16:22:02 +0200

    Don't fire responsive nav by default.

--- scripts/main.js
+++ scripts/main.js
@@ -6,19 +6,19 @@ $(function() {
 	});
 
     // fire Responsive Nav
-    var navigation = responsiveNav('#primary-nav', {
-        animate: true,          // Boolean: Use CSS3 transitions, true or false
-        transition: 250,        // Integer: Speed of the transition, in milliseconds
-        label: "Navigation",    // String: Label for the navigation toggle
-        insert: "before",       // String: Insert the toggle before or after the navigation
-        customToggle: "",       // Selector: Specify the ID of a custom toggle
-        openPos: "relative",    // String: Position of the opened nav, relative or static
-        jsClass: "js",          // String: 'JS enabled' class which is added to <html> el
-        debug: false,           // Boolean: Log debug messages to console, true or false
-        init: function(){},     // Function: Init callback
-        open: function(){},     // Function: Open callback
-        close: function(){}     // Function: Close callback
-    });
+    // var navigation = responsiveNav('#primary-nav', {
+    //     animate: true,          // Boolean: Use CSS3 transitions, true or false
+    //     transition: 250,        // Integer: Speed of the transition, in milliseconds
+    //     label: "Navigation",    // String: Label for the navigation toggle
+    //     insert: "before",       // String: Insert the toggle before or after the navigation
+    //     customToggle: "",       // Selector: Specify the ID of a custom toggle
+    //     openPos: "relative",    // String: Position of the opened nav, relative or static
+    //     jsClass: "js",          // String: 'JS enabled' class which is added to <html> el
+    //     debug: false,           // Boolean: Log debug messages to console, true or false
+    //     init: function(){},     // Function: Init callback
+    //     open: function(){},     // Function: Open callback
+    //     close: function(){}     // Function: Close callback
+    // });
 
     // JS PLUGINS LAZY-LOADING
     // @media threshold
