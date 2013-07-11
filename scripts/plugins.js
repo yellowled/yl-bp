51dbf0934e233aa1320c428f1948b8c4fc109b77
Matthias Mees <mm@yellowled.de>
2013-07-11 23:19:09 +0200

    Add manual shiv for main element.

--- scripts/plugins.js
+++ scripts/plugins.js
@@ -22,6 +22,12 @@
 }());
 
 
+// TEMP shiv main element manually
+// http://drublic.de/blog/add-main-element-modernizr/
+window.html5.elements = 'main';
+window.html5.shivDocument(document);
+
+
 /*
  * Accessifyhtml5.js
  *
