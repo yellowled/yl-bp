d40de6448340c64453087ac6c8a8e4d4ebe1eaed
Matthias Mees <mm@yellowled.de>
2014-03-12 12:37:48 +0100

    Use more explicit target

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
         'uglify',
         'imagemin:deploy',
         'favicons:deploy',
-        'modernizr:deploy',
+        'modernizr',
         'hashres:deploy'
     ]);
 };
