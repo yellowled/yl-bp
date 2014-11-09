e2d69ab202b8c607496b96cb1bcc93af5f1e1699
Matthias Mees <mm@yellowled.de>
2014-11-09 15:24:09 +0100

    Add basic support for smallscreen nav.
    - Add JS for nav toggler (commented out).
    - Add basic nav styles + variables for that.
    - Add toggler + state attributes to nav include.

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,20 @@ $(function() {
 		header: 'body > header',
 		footer: 'body > footer'
 	});
+
+    // Smallscreen navigation
+    // $('.toggle-nav').click(function(e) {
+    //     var $el = $(this);
+    //     var navTarget = $el.attr('href')
+    //     var navState = $(navTarget).attr('aria-hidden');
+
+    //     $(navTarget).toggleClass('collapsed');
+    //     if (navState == 'true') {
+    //         $(navTarget).attr('aria-hidden', 'false');
+    //     } else {
+    //         $(navTarget).attr('aria-hidden', 'true');
+    //     }
+
+    //     e.preventDefault();
+    // });
 });
