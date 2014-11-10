31792d335c1c325bca04dd13ef551962a580dc75
Matthias Mees <mm@yellowled.de>
2014-11-10 14:50:54 +0100

    Improved implementation of smallscreen nav.

--- scripts/main.js
+++ scripts/main.js
@@ -6,18 +6,21 @@ $(function() {
 	});
 
     // Smallscreen navigation
-    // $('.toggle-nav').click(function(e) {
-    //     var $el = $(this);
-    //     var navTarget = $el.attr('href')
-    //     var navState = $(navTarget).attr('aria-hidden');
+    if ($('.nav-toggle').length > 0) {
+        $('.nav-toggle').click(function(e) {
+            var $el = $(this);
+            var navTarget = $el.attr('href')
+            var navState = $(navTarget).attr('aria-hidden');
 
-    //     $(navTarget).toggleClass('collapsed');
-    //     if (navState == 'true') {
-    //         $(navTarget).attr('aria-hidden', 'false');
-    //     } else {
-    //         $(navTarget).attr('aria-hidden', 'true');
-    //     }
+            $(navTarget).toggleClass('nav-collapsed');
 
-    //     e.preventDefault();
-    // });
+            if (navState == 'true') {
+                $(navTarget).attr('aria-hidden', 'false');
+            } else {
+                $(navTarget).attr('aria-hidden', 'true');
+            }
+
+            e.preventDefault();
+        });
+    }
 });
