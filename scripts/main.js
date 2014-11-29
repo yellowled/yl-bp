8a3f713e7dac9c5aeb765cc940a1e4fd45413129
Matthias Mees <mm@yellowled.de>
2014-11-29 21:05:45 +0100

    Remove "solution" for smallscreen navigation.
    We need a better, more flexible solution for this.

--- scripts/main.js
+++ scripts/main.js
@@ -4,23 +4,4 @@ $(function() {
 		header: 'body > header',
 		footer: 'body > footer'
 	});
-
-    // Smallscreen navigation
-    if ($('.nav-toggle').length > 0) {
-        $('.nav-toggle').click(function(e) {
-            var $el = $(this);
-            var navTarget = $el.attr('href');
-            var navState = $(navTarget).attr('aria-hidden');
-
-            $(navTarget).toggleClass('nav-collapsed');
-
-            if (navState == 'true') {
-                $(navTarget).attr('aria-hidden', 'false');
-            } else {
-                $(navTarget).attr('aria-hidden', 'true');
-            }
-
-            e.preventDefault();
-        });
-    }
 });
