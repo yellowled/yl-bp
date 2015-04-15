102b73392bfa002700991bb22b057fd30f0e2e42
Matthias Mees <mm@yellowled.de>
2015-04-15 15:17:28 +0200

    Redundant call to AccessifyHTML5
    See 837b882f8bd0f3cc14e791381ddc7171261f3730

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,4 @@
 $(function() {
-	// fire AccessifyHTML5
-	AccessifyHTML5({
-		header: 'body > header',
-		footer: 'body > footer'
-	});
-
     // Smallscreen nav
     var nav = responsiveNav('.nav-collapse', {
         customToggle: "#open-nav",
