3c8b26232a2c7d2da8301af44daa57f6d9a0e887
Matthias Mees <mm@yellowled.de>
2015-06-08 11:51:52 +0200

    Add and use .jshintrc

--- grunt/contrib-jshint.js
+++ grunt/contrib-jshint.js
@@ -1,6 +1,9 @@
 // Check JS assets for code quality
 module.exports = function(grunt) {
     grunt.config('jshint', {
+        options: {
+            jshintrc: true
+        },
         all: ['Gruntfile.js',
               'grunt/*.js',
               'scripts/main.js']
