d05ba32ff847cb50a21bd331ea6e26b8ea0b0fb9
Matthias Mees <mm@yellowled.de>
2015-10-16 12:45:21 +0200

    Add sourcemaps for master JS file (dev + production)

--- grunt/contrib-concat.js
+++ grunt/contrib-concat.js
@@ -3,6 +3,9 @@ var config = require('../config');
 
 module.exports = function(grunt) {
     grunt.config('concat', {
+        options: {
+            sourceMap: true
+        },
         master: {
             src: config.concat.src,
             dest: config.concat.dest
--- grunt/contrib-uglify.js
+++ grunt/contrib-uglify.js
@@ -1,6 +1,9 @@
 // Minify JS
 module.exports = function(grunt) {
     grunt.config('uglify', {
+        options: {
+            sourceMap: true
+        },
         deploy: {
             src: 'src/scripts/master.js',
             dest: 'dist/scripts/master.js'
