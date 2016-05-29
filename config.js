65629ed1826f49a53a30edf41f2d599865eaba7c
Matthias Mees <mm@yellowled.de>
2016-05-29 17:54:29 +0200

    Replace jQuery-Collapse with fr-accordion
    References #48

--- config.js
+++ config.js
@@ -12,8 +12,7 @@ module.exports = {
                     'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
                     'node_modules/slick-carousel/slick/slick.js',
                     'node_modules/fr-tabs/dist/frtabs.min.js',
-                    'node_modules/jquery-collapse/src/jquery.collapse.js',
-                    'node_modules/jquery-collapse/src/jquery.collapse_storage.js',
+                    'node_modules/fr-accordion/dist/fraccordion.min.js',
                     'src/scripts/main.js'
               ],
         dest:       'src/scripts/master.js'
