3c6d9e3bb895248600eed50394e676130d538623
Matthias Mees <mm@yellowled.de>
2019-11-02 22:52:49 +0100

    Modularize domready function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,4 @@
-var ready = function(fn) {
-    if (typeof fn !== 'function') return;
-    // If document is already loaded, run method
-    if (document.readyState === 'complete') {
-        return fn();
-    }
-    // Otherwise, wait until document is loaded
-    document.addEventListener('DOMContentLoaded', fn, false);
-};
+import ready from './util/domready';
 
 ready(function() {
     // Here be dragons
---
+++ src/scripts/util/domready.js
@@ -0,0 +1,11 @@
+const ready = function(fn) {
+    if (typeof fn !== 'function') return;
+    // If document is already loaded, run method
+    if (document.readyState === 'complete') {
+        return fn();
+    }
+    // Otherwise, wait until document is loaded
+    document.addEventListener('DOMContentLoaded', fn, false);
+};
+
+export default ready;
